d3a91ef07ec38dca879a7f64fc5eeab025bfb53b
Kevin Li <71240367+kevzli@users.noreply.github.com>
2024-11-04 16:31:54 -0800

    update
    update

---
+++ SoloSync/SelectedPoint.swift
@@ -0,0 +1,22 @@
+//
+//  SelectedPoint.swift
+//  SoloSync
+//
+//  Created by Kevin Li on 11/4/24.
+//
+
+import Foundation
+import MapKit
+
+class SelectedPoint: NSObject, MKAnnotation{
+    
+    let title: String?
+    let location: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String?, location: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.location = location
+        self.coordinate = coordinate
+    }
+}
--- SoloSync/ViewController.swift
+++ SoloSync/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var switchValue: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeMap(_ sender: Any) {
+        if (switchValue.isOn){
+            mapView.mapType = .satellite
+        } else{
+            mapView.mapType = .standard
+        }
+    }
+    
+    @IBAction func addPin(_ sender: Any) {
+        
+    }
 }
 

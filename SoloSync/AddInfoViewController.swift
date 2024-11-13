50d2a0fee0570d809f8faa373630263f245c0480
Kevin Li <71240367+kevzli@users.noreply.github.com>
2024-11-13 12:52:17 +0800

    updated add annotation UI

--- SoloSync/AddInfoViewController.swift
+++ SoloSync/AddInfoViewController.swift
@@ -1,50 +1,95 @@
-//
-//  AddInfoViewController.swift
-//  SoloSync
-//
-//  Created by Kevin Li on 11/5/24.
-//
-
-import Foundation
 import UIKit
 import CoreLocation
 
 class AddInfoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var coordinate: CLLocationCoordinate2D?
-    var noteTextField: UITextField!
+    var noteTextView: UITextView!
     var selectedImage: UIImage?
+    var imageView: UIImageView!
+    var addImageButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        setupTextField()
-        setupAddImage()
+        setupImageView()
+        setupAddImageButton()
+        setupNoteTextView()
         setupSaveButton()
     }
 
-    private func setupTextField() {
-        noteTextField = UITextField(frame: CGRect(x: 20, y: 100, width: view.frame.width - 50, height: 50))
-        noteTextField.placeholder = "Add Note"
-        noteTextField.borderStyle = .roundedRect
-        view.addSubview(noteTextField)
+    private func setupImageView() {
+        imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = UIColor.lightGray
+        imageView.layer.cornerRadius = 8
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        view.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3)
+        ])
     }
 
-    private func setupAddImage() {
-        let addImageButton = UIButton(type: .system)
-        addImageButton.frame = CGRect(x: 20, y: 170, width: view.frame.width - 50, height: 50)
-        addImageButton.setTitle("Add Photo", for: .normal)
+    private func setupAddImageButton() {
+        addImageButton = UIButton(type: .system)
+        addImageButton.translatesAutoresizingMaskIntoConstraints = false
+        addImageButton.setImage(UIImage(systemName: "plus.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 40, weight: .bold)), for: .normal)
+        addImageButton.tintColor = .systemBlue
         addImageButton.addTarget(self, action: #selector(presentImagePicker), for: .touchUpInside)
         view.addSubview(addImageButton)
+        
+        NSLayoutConstraint.activate([
+            addImageButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 15),
+            addImageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            addImageButton.widthAnchor.constraint(equalToConstant: 60),
+            addImageButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+
+    private func setupNoteTextView() {
+        noteTextView = UITextView()
+        noteTextView.translatesAutoresizingMaskIntoConstraints = false
+        noteTextView.layer.cornerRadius = 8
+        noteTextView.layer.masksToBounds = true
+        noteTextView.backgroundColor = .systemGray6
+        noteTextView.font = UIFont.systemFont(ofSize: 16)
+        noteTextView.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        noteTextView.text = "Add Note"
+        noteTextView.textColor = .gray
+        
+        noteTextView.delegate = self
+        view.addSubview(noteTextView)
+        
+        NSLayoutConstraint.activate([
+            noteTextView.topAnchor.constraint(equalTo: addImageButton.bottomAnchor, constant: 20),
+            noteTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            noteTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            noteTextView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2)
+        ])
     }
 
     private func setupSaveButton() {
         let saveButton = UIButton(type: .system)
-        saveButton.frame = CGRect(x: 20, y: 240, width: view.frame.width - 50, height: 50)
-        saveButton.setTitle("Save", for: .normal)
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        saveButton.setTitle("Post", for: .normal)
+        saveButton.setTitleColor(.white, for: .normal)
+        saveButton.backgroundColor = .systemBlue
+        saveButton.layer.cornerRadius = 20
         saveButton.addTarget(self, action: #selector(saveInfo), for: .touchUpInside)
         view.addSubview(saveButton)
+        
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: noteTextView.bottomAnchor, constant: 20),
+            saveButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            saveButton.widthAnchor.constraint(equalToConstant: 100),
+            saveButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
     @objc func presentImagePicker() {
@@ -55,7 +100,7 @@ class AddInfoViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
 
     @objc func saveInfo() {
-        guard let coordinate = coordinate, let note = noteTextField.text, !note.isEmpty else {
+        guard let coordinate = coordinate, let note = noteTextView.text, !note.isEmpty, note != "Add Note" else {
             return
         }
 
@@ -67,7 +112,24 @@ class AddInfoViewController: UIViewController, UIImagePickerControllerDelegate, 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             selectedImage = image
+            imageView.image = image
         }
         picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension AddInfoViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == .gray {
+            textView.text = ""
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Add Note"
+            textView.textColor = .gray
+        }
     }
 }
